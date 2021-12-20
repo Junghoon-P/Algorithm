d1090321a28b8a253d12202b9e4452e01d0fe522
Junghoon-P <qkr0454@naver.com>
2021-12-21 08:48:41 +0900

    set을 만드는 방법

---
+++ contents/Algorithms/JavaScript/Js 100제/21.js
@@ -0,0 +1,12 @@
+// 다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+// 1)  var x = {1, 2, 3, 5, 6, 7};
+// 2)  var x = {};
+// 3)  var x = new Set('javascript');
+// 4)  var x = new Set(range(5));
+// 5)  var x = new Set();
+
+// 정답;
+// Set 객체는 자료형에 관계 없이 원시 값과 객체 참조 모두 유일한 값을 저장할 수 있습니다.
+// 3)  var x = new Set('javascript');
+// 5)  var x = new Set();
