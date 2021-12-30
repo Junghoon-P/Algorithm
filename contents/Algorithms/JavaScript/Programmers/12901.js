093c5ec92478bd666ff84019cf118d1961f631c8
Junghoon-P <qkr0454@naver.com>
2021-12-30 18:53:01 +0900

    12901 : 2016년

--- contents/Algorithms/JavaScript/Programmers/12901.js
+++ contents/Algorithms/JavaScript/Programmers/12901.js
@@ -2,7 +2,7 @@
 function solution(a, b) {
   let month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   let day = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
-  let JanFirst = 3;
+  const JanFirst = 3;
   let sum = JanFirst + b;
   let currentMonth = month.splice(0, a);
   for (let i = 0; i < currentMonth.length; i++) {
