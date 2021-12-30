fd6c57113df98ee8f88372e53c9c5ae04be0269e
Junghoon-P <qkr0454@naver.com>
2021-12-30 18:09:03 +0900

    12901 : 2016년 풀이 수정

--- contents/Algorithms/JavaScript/Programmers/12901.js
+++ contents/Algorithms/JavaScript/Programmers/12901.js
@@ -2,10 +2,11 @@
 function solution(a, b) {
   let month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   let day = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
-  let JanFirst = 3; //idx값을 위해 -1 미리 해놓음
+  let JanFirst = 3;
   let sum = JanFirst + b;
-  for (let i = 0; i < month.length; i++) {
-    sum += month[i];
+  let currentMonth = month.splice(0, a);
+  for (let i = 0; i < currentMonth.length; i++) {
+    sum += currentMonth[i];
   }
   return day[sum % 7];
 }
