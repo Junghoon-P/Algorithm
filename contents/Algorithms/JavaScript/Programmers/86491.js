43a21e2a4cae1953a813ccc1d1f7fd994c788019
Junghoon-P <qkr0454@naver.com>
2022-01-02 22:38:15 +0900

    86491 : 최소직사각형

--- contents/Algorithms/JavaScript/Programmers/86491.js
+++ contents/Algorithms/JavaScript/Programmers/86491.js
@@ -1,4 +1,28 @@
 function solution(sizes) {
-  var answer = 0;
-  return answer;
+  let width = [];
+  let height = [];
+
+  for (let i = 0; i < sizes.length; i++) {
+    sizes[i].sort((a, b) => a - b);
+  }
+
+  for (let i = 0; i < sizes.length; i++) {
+    width.push(sizes[i][0]);
+    height.push(sizes[i][1]);
+  }
+
+  return Math.max(...width) * Math.max(...height);
+}
+
+//
+function solution(sizes) {
+  let w = 0;
+  let h = 0;
+  sizes.forEach((s) => {
+    const [a, b] = s.sort((a, b) => a - b);
+    if (a > h) h = a;
+    if (b > w) w = b;
+  });
+
+  return w * h;
 }
