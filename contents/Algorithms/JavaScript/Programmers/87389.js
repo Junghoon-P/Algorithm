92bf49cdc7e550a908c4af4494d8fa404aa056ae
Junghoon-P <qkr0454@naver.com>
2022-01-03 00:00:24 +0900

    87389 : 다른 사람의 풀이

---
+++ contents/Algorithms/JavaScript/Programmers/87389.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  for (let i = 0; i < n; i++) {
+    if (n % i === 1) {
+      return i;
+    }
+  }
+}
+
+// 다른 풀이
+function solution(n, x = 1) {
+  while (x++) {
+    if (n % x === 1) {
+      return x;
+    }
+  }
+}
